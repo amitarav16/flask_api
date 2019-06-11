12754156a93eb4333c717cd0f56634d7562b9d86
Amit Kumar Singh <amitkumar26201@gmail.com>
2019-06-11 17:20:44 +0530

    Add files via upload

---
+++ google_App_engine/weather_api/weather.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import os
+import pytz
+import requests
+import math
+API_KEY = 'e7910b8aedfb69b074f8bf03d0025f5f'
+API_URL = ('http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=metric&appid={}')
+def query_api(city):
+    try:
+        print(API_URL.format(city, API_KEY))
+        data = requests.get(API_URL.format(city, API_KEY)).json()
+    except Exception as exc:
+        print(exc)
+        data = None
+    return data
